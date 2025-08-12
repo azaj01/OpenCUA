8d6f50652578e498eaa1055be4ea364e2ecc11cc
ludunjie1219 <ludj1219@gmail.com>
2025-08-12 17:37:32 +0800

    refactor

--- DataProcessor/scripts/extract_raw.sh
+++
@@ -1,3 +0,0 @@
-DATASET=$1
-NUM_SAMPLES=${2:-1}
-python -m gui_agent_data.stage.extract_raw $DATASET datasets/$DATASET/raw_trajs -n $NUM_SAMPLES
--- DataProcessor/scripts/raw_to_standardized.sh
+++
@@ -1,4 +0,0 @@
-DATASET=$1
-NUM_SAMPLES=${2:-"-1"}
-
-python -m gui_agent_data.stage.raw_to_standardized $DATASET datasets/$DATASET/raw_trajs datasets/$DATASET/standardized --num_samples $NUM_SAMPLES
